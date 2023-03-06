401dd6dd4675501c9f06e2b1644dca20325ef2cc
lokesh kapse <lkapse63@gmail.com>
2023-03-06 20:16:46 +0530

    inner classes demo

--- src/oop/classes.py
+++ src/oop/classes.py
@@ -1,10 +1,24 @@
 """ This is first line use as muiltline comment"""
+import json
+from json import JSONEncoder
 
 
 class Person:
-    pass
+     
+    def __init__(self, name: str, age: float, address: str) -> None:
+        self.name = name
+        self.age = age
+        self.address = address  
+        
+    def __repr__(self) -> str:
+        return json.dumps(self, cls=PersonEncoder)
 
 
+class PersonEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+   
 class Employee:
     pass
 
@@ -13,5 +27,34 @@ class Business:
     pass
 
 
-p1 = Person()
+p1 = Person("lucky", 32.1, "pune")
 print(p1)
+print(p1.__dict__["address"])
+
+
+# Inner class
+class Outer:
+    
+    def __init__(self, outer_var, inner_var_in) -> None:
+        self.var = outer_var
+        self.inner_obj = self.Inner(inner_var_in)
+     
+    def __str__(self) -> str:
+        return str(self.__dict__)   
+    
+    def get_inner(self):
+        return self.inner_obj._get_inner()
+        
+    # Inner class
+    class Inner:
+        
+        def __init__(self, inner_var) -> None:
+            self.inner_var = inner_var
+            
+        def _get_inner(self):
+            return self.inner_var
+            
+
+outer = Outer('outer', 'inner')
+
+print(outer.get_inner())
